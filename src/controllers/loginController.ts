1ea52bafadf12a479d3a6d3a8a2cb3054e5a213c
mahimag <mahimag>
2022-09-29 17:34:12 +0545

    import refactor

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-
 import * as userService from "../services/userService";
 
 export const signin = (req: Request, res: Response, next: NextFunction) => {
